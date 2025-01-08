03b1f96fae55c22073fc2e848a9fa2351fa64630
NoahKrobot <D00259927@student.dkit.ie>
2025-01-08 00:55:22 +0000

    Sending strength and date on pubnub
    In this commit I figured out how to send date and strength to pubnub from my software.
    
    Those updates are required for the project.

--- static/pubnub/pubnub.js
+++ static/pubnub/pubnub.js
@@ -1,7 +1,26 @@
-const chat = await Chat.init({
+let pubnub
+
+async function setupPubNub(){
+  const strengthValue = document.getElementById("strengthSlider").value;
+  pubnub = new PubNub({
     publishKey: "pub-c-6c61f0f4-a610-4bc4-a4b8-72c7a57d5fd1",
     subscribeKey: "sub-c-a7c449e1-c9ca-484c-b992-2b146e8a0029",
-    userId,
-    typingTimeout: 2000,
-  })
+    userId: "noah_id",
+  });
+
+  const messageStr = {                  
+    text: strengthValue,  
+    timestamp: new Date().toISOString() 
+  };
+
+  pubnub.publish(
+    {
+      channel: "strength_ch",
+      message: messageStr
+    }
+  );
+  
+}
+
+
   
