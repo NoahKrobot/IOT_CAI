b914d0279ed6dcf0ffda7039491e5f28f353f555
NoahKrobot <D00259927@student.dkit.ie>
2025-01-08 14:54:58 +0000

    databaseHandler.js written
    In this commit I populated databaseHandler.js with required code to connect it to Firebase Firestore. I've also included some files within index.html.
    
    Those updates are required for the project.

---
+++ static/js/databaseHandler.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYArcypqqY73wFtwGrzlegjyhkhRiuzuM",
+  authDomain: "lightbook-24211.firebaseapp.com",
+  projectId: "lightbook-24211",
+  storageBucket: "lightbook-24211.firebasestorage.app",
+  messagingSenderId: "245779430190",
+  appId: "1:245779430190:web:bdb631444719512e1b58bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
