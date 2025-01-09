a63c6180e4dc02e03af07b9143197cd24cc0417f
NoahKrobot <D00259927@student.dkit.ie>
2025-01-09 19:45:58 +0000

    PubNub modifies LED display
    In this commit I wrote python code to turn led displays on if there's a message on PubNub
    
    Those updates are required for the project.

--- pubnubPiListener.py
+++ pubnubPiListener.py
@@ -1,6 +1,14 @@
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.pubnub import PubNub
 from pubnub.callbacks import SubscribeCallback
+from gpiozero import LED
+led2 = LED(2)
+led3 = LED(3)
+led4 = LED(4)
+led17 = LED(17)
+led22 = LED(22)
+led27 = LED(27)
+led10 = LED(10)
 
 pnconfig = PNConfiguration()
 pnconfig.subscribe_key = "sub-c-a7c449e1-c9ca-484c-b992-2b146e8a0029"
@@ -10,10 +18,16 @@ pubnub = PubNub(pnconfig)
 
 class MySubscribeCallback(SubscribeCallback):
     def message(self, pubnub, message):
-        print(f"msg: {message.message}")
-    
+        print(f"Received message: {message.message}")
+        led2.on()
+        led3.on()
+        led4.on()
+        led17.on()
+        led22.on()
+        led27.on()
+        led10.on()
 pubnub.add_listener(MySubscribeCallback())
 pubnub.subscribe().channels("strength_ch").execute()
 
-print("Listening...")
+print("Listening")
 
